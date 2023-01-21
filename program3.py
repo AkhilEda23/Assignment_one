82af352d3e7a713335fff6c5fd0aedc73618406c
AkhilEda23 <sxe74810@ucmo.edu>
2023-01-21 16:25:37 -0600

    third commit

---
+++ program3.py
@@ -0,0 +1,25 @@
+#TUPLE
+
+#Created brother tuple
+brother_tuple = ("adam","jamshed","jalal","arsal","farhaad")
+
+#Created sister tuple
+sister_tuple = ("ajiya","kinza","huma","shahana")
+
+#Joined brothers and sister tuple and assigned to sibligs
+siblings_tuple = brother_tuple + sister_tuple
+
+#Finding the length of siblings tuple
+print(len(siblings_tuple))
+
+
+#Creating parents tuple
+parents_tuple =("Nazakath Ali","Mumtaz Begum")
+
+#joining parents and siblings tuple to a new tuple family_tuplle
+family_tuple = siblings_tuple + parents_tuple
+
+print("Family: ",family_tuple)
+
+
+
